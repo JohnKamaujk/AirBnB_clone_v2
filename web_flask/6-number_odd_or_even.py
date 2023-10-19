574259a537d151c957543be0a2dc56105a1a66a8
JohnKamaujk <johnjkkamau@gmail.com>
2023-10-19 17:13:44 +0300

    :sparkles:chore:number_odd_even/n route

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -10,10 +10,12 @@ Routes:
     /c/<text>: Displays “C ” followed by the value of the text variable.
     /python: Displays “Python is cool” message.
     /python/<text>: Displays “Python” followed by the value of text variable.
-    /number/<n>: Displays “n is a number” only if n is an intege
+    /number/<n>: Displays “n is a number” only if n is an integer.
+    /number_template/<n>: Displays an HTML page only if <n> is an integer.
+    /number_odd_or_even/<n>: display a HTML page only if n is an integer.
 """
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -47,6 +49,18 @@ def python_is_cool(text="is cool"):
 def number(n):
     """Displays 'n is a number' if n is an integer."""
     return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """Displays an HTML page only if <n> is an integer."""
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_even_template(n):
+    """Displays an HTML page only if n is an integer."""
+    return render_template("6-number_odd_or_even.html", n=n)
 
 
 if __name__ == "__main__":
