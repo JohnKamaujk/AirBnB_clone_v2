f6bd745353fe1acf818fc93440b4f14f39c543a0
JohnKamaujk <johnjkkamau@gmail.com>
2023-10-19 13:30:37 +0300

    :sparkles:chore:c/<text> route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,7 +6,8 @@ The application listens on 0.0.0.0, port 5000.
 
 Routes:
     /: Displays a friendly 'Hello HBNB!' message.
-    /hbnb: Displays a friendly 'HBNB' message
+    /hbnb: Displays a friendly 'HBNB' message.
+    /c/<text>: Displays “C ” followed by the value of the text variable.
 """
 
 from flask import Flask
@@ -24,6 +25,12 @@ def hello_hbnb():
 def hbnb():
     """Displays a friendly 'HBNB' message"""
     return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    """Displays “C ” followed by the value of the text variable"""
+    return "C " + text.replace("_", " ")
 
 
 if __name__ == "__main__":
